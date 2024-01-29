781c3bd891ff9cc68def26ab7e24000fc76b9b7c
unknown <guilherme.studier18@gmail.com>
2024-01-28 21:19:21 -0300

    add loading

--- App.tsx
+++ App.tsx
@@ -1,7 +1,18 @@
+import { ThemeProvider } from 'styled-components'
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+
+import theme from './src/theme'
 import { Groups } from '@screens/Groups'
 
+import { Loading } from '@components/Loading'
+
+
 export default function App() {
+  const [fontsLoad] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
   return (
-    <Groups />
+    <ThemeProvider theme={theme}>
+      { !fontsLoad ? <Groups /> : <Loading />}
+    </ThemeProvider>
   );
 }
