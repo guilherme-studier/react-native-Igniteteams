574fd9d45ea160d28dbab6b518c55e9afa0c1f3c
unknown <guilherme.studier18@gmail.com>
2024-01-29 23:04:44 -0300

    add to Button components

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -5,6 +5,7 @@ import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 import { Container } from './styles';
 
@@ -30,6 +31,10 @@ export function Groups() {
             message='Que tal cadastrar a primeira turma?'
           />
          )}
+      />
+
+      <Button 
+        title='Criar nova turma'
       />
     </Container>
   );
