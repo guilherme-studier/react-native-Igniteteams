d8ffc92c5032bfe222b30771d8cfdcfa1ff2dbba
unknown <guilherme.studier18@gmail.com>
2024-01-31 14:32:41 -0300

    add filter player

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,11 +1,18 @@
-import { Container, Form } from './styles'
+import { useState } from 'react'
+import { FlatList } from 'react-native'
 
-import { Header } from '@components/Header'
-import { Highlight } from '@components/Highlight'
+import { Container, Form, HeaderList, NumbersOfPalyers } from './styles'
+
 import { ButtonIcon } from '@components/ButtonIcon'
+import { Highlight } from '@components/Highlight'
+import { Header } from '@components/Header'
+import { Filter } from '@components/Filter'
 import { Input } from '@components/Input'
 
 export function Players() {
+  const [team, setTeam] = useState('Time A')
+  const [players, setPlayers] = useState(['Guilherme', 'Marina', 'Thiago'])
+
   return(
     <Container>
       <Header showBackButton/>
@@ -25,6 +32,27 @@ export function Players() {
           icon="add" 
         />
       </Form>
+
+      <HeaderList>
+        <FlatList 
+          data={['Time A', 'Time B']}
+          keyExtractor={item => item}
+          renderItem={({ item}) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+        <NumbersOfPalyers>
+          {players.length}
+        </NumbersOfPalyers>
+
+      </HeaderList>
+
+
       
     </Container>
   )
